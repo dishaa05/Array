b72d2ef1be1ce54bf38c9acedf93a39c8a7a2034
dishaa05 <dishajhinjhore0505@gmail.com>
2026-03-01 20:29:58 +0530

    Array

---
+++ p1.cpp
@@ -0,0 +1,19 @@
+//Problem 1: Input and Display Array
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int number[5];
+    cout<<"enter 5 numbers;";
+    for(int i=0; i<5; i++){
+       cin>>number[i];
+    }
+
+
+    for(int i=0; i<5; i++){
+        cout<<
+        cout<<number[i];
+    }
+}
