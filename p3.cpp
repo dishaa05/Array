f8716e28c2676b5b60ebc6b77113794d66f0e13b
dishaa05 <dishajhinjhore0505@gmail.com>
2026-03-03 16:42:50 +0530

    Array

---
+++ p3.cpp
@@ -0,0 +1,19 @@
+//Problem 3: Sum of Array Elements
+
+#include<iostream>
+using namespace std;
+
+int main () {
+    int array[5];
+    int sum = 0;
+
+    for(int i=0; i<5; i++) {
+        cout << "Enter number " << i+1 << ": ";
+        cin >> array[i];
+        sum += array[i];
+    }
+
+    cout << "Sum of array elements: " << sum << endl;
+
+    return 0;
+}
