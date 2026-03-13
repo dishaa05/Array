f37417e30a0446d2316dbda1678d28ea129859c4
dishaa05 <dishajhinjhore0505@gmail.com>
2026-03-13 23:32:41 +0530

    Array

---
+++ p12.cpp
@@ -0,0 +1,17 @@
+//Problem 12: Sum of Odd Numbers
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int array[5] = {1, 2, 3, 4, 5};
+    int sum=0;
+    for(int i=0; i<5; i++){
+        if(array[i]%2!=0){
+            sum+=array[i];
+        }
+    }
+    cout<<"Sum of Odd numbers:"<<sum;
+    return 0;
+}
