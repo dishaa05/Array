e0c17aa711a23d511c11a3dbde65128cb2515a0b
dishaa05 <dishajhinjhore0505@gmail.com>
2026-03-24 22:59:08 +0530

    Array

---
+++ p20.cpp
@@ -0,0 +1,16 @@
+//Problem 20: Reverse Array (In-Place)
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[5]={1,2,3,4,5};
+    int n=5;
+    for(int i=0;i<n/2;i++){
+        swap(arr[i],arr[n-i-1]);
+    }
+    cout<<"Reversed Array=";
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
